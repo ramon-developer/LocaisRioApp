1c6ddb0889cc2e7238db0bef987d8a1528e40889
Ramon Santos <ramon.programmer@gmail.com>
2017-05-14 00:11:06 -0300

    Initial Commit

---
+++ LocaisRioApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocaisRioApp
+//
+//  Created by Ramon dos Santos on 14/05/17.
+//  Copyright © 2017 Ebix American Latin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
